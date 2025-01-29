9b66b195a95a371fa3e8ee5caacc5d97bf2a66dd
Michael Gomeh <gomehmichael@gmail.com>
2025-01-29 23:42:06 +0200

    base home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 const App = () => (
-	<main>
-		<div>Main</div>
-	</main>
+	<div className="home">
+		<header>
+			My portfolio
+			<nav>
+				<a href="#">Home</a>
+				<a href="#">Projects</a>
+				<a href="#">Contant</a>
+			</nav>
+		</header>
+		<main>
+			<div>Main</div>
+			<footer>hi</footer>
+		</main>
+	</div>
 );
 
 export default App;
