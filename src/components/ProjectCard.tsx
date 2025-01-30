153d560636862c9f7979d5e113bda7b44f52e9b4
Michael Gomeh <gomehmichael@gmail.com>
2025-01-31 01:27:26 +0200

    project card structure

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,11 @@
 const ProjectCard = ({ title, description, stack, url }) => {
-	return <div>ProjectCard</div>;
+	return (
+		<div className="card flex col center">
+			<div className="project-title">{title}</div>
+			<div className="project-description">{description}</div>
+			<div className="project-stack">{stack}</div>
+		</div>
+	);
 };
 
 export default ProjectCard;
