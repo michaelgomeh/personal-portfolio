9f0b50eddc1e6212b456ab19b6e04248f3d7ca33
Michael Gomeh <gomehmichael@gmail.com>
2025-01-30 11:01:44 +0200

    main content layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
-const App = () => (
-	<div className="home">
-		<header>
-			My portfolio
-			<nav>
-				<a href="#">Home</a>
-				<a href="#">Projects</a>
-				<a href="#">Contant</a>
-			</nav>
-		</header>
-		<main>
-			<div>Main</div>
+import { useState } from 'react';
+import {
+	IconBrandGithubFilled,
+	IconBrandLinkedinFilled,
+	IconWorldWww,
+} from '@tabler/icons-react';
+const App = () => {
+	const [drawerOpen, setDrawerOpen] = useState(false);
+	return (
+		<div className="home">
+			<header>
+				My portfolio
+				<button
+					className="drawerToggle"
+					data-visible={drawerOpen}
+					onClick={() => setDrawerOpen(!drawerOpen)}
+				></button>
+				<nav data-visible={drawerOpen}>
+					<a href="#">Home</a>
+					<a href="#">Projects</a>
+					<a href="#">Contant</a>
+				</nav>
+			</header>
+			<div className="social">
+				<a href="">
+					<IconBrandGithubFilled strokeWidth={1} />
+				</a>
+				<a href="">
+					<IconBrandLinkedinFilled strokeWidth={1} />
+				</a>
+				<a href="">
+					<IconWorldWww strokeWidth={1} />
+				</a>
+			</div>
+			<main>
+				<div>hero-text</div>
+				<div>picture</div>
+				<div>content</div>
+			</main>
 			<footer>hi</footer>
-		</main>
-	</div>
-);
+		</div>
+	);
+};
 
 export default App;
