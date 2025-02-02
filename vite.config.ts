b0476afb83c87d5a894c1998e86cafa88378c735
Michael Gomeh <gomehmichael@gmail.com>
2025-02-02 10:55:42 +0200

    fixing package and vite config urls

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+	base: '/personal-portfolio/',
 	resolve: {
 		alias: {
 			'@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
-			base: '/personal-portfolio/',
 		},
 	},
 });
