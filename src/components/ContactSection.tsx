9fde413318bffb4ecddcd9b633a556d8632d3612
Michael Gomeh <gomehmichael@gmail.com>
2025-02-23 19:09:37 +0200

    move into dist dir

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const ContactSection = () => {
 	return <div>ContactSection</div>;
 };
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export interface IProjectCardProps {
 	title: string;
 	description: string;
