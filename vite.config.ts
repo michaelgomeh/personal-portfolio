cc8a2e42531966956ab005e0c7a0019fac2343d8
Michael Gomeh <gomehmichael@gmail.com>
2025-02-26 00:33:26 +0200

    removed gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/personal-portfolio/',
 	resolve: {
 		alias: {
 			'@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
